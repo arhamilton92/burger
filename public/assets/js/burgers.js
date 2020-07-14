82aab396f09969ff431c017c342096b32e7e26e0
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-14 18:27:55 -0400

    fixed burgers.js devour typo

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,44 +1,45 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-devoured").on("click", function(event) {
-      console.log('click!')
-        var id = $(this).data("id");
-        var newDevoured = true;
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevoured");
 
-        var newDevouredState = {
-        devoured: newDevoured
-        };
+    var newDevourState = {
+      devour: newDevour
+    };
 
-      // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-        }).then(
-        function() {
-            console.log("changed devoured to", newDevoured);
-            // Reload the page to get the updated list
-            location.reload()
-        })
-    });
-
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        name: $("#ca").val().trim(),
-        devoured: $("[name="devoured"]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("creating burger....");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("changed devour to", newDevour);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
   });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newCat = {
+      name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
