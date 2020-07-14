2e6c4cbd0dcd5e6b66624ee015454e660987b7dc
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-14 17:33:41 -0400

    fixed connection error

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@
 var mysql = require("mysql");
 // ----------
 if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    const connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     const connection = mysql.createConnection({
         host: "localhost",
