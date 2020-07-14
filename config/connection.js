33e292ccdd8ecd71a606e1776105e142a672c482
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-14 16:56:06 -0400

    setup JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,17 @@
 // ========================================================
 var mysql = require("mysql");
 // ----------
-const connection = mysql.createConnection({
-    host: "localhost",
-    // Your port; if not 3306
-    port: 3306,
-    // Your username
-    user: "root",
-    // Your password
-    password: "EkUF,wC,3^~9&WXfXJ*.4X~i",
-    database: "burgersDB"
-});
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "EkUF,wC,3^~9&WXfXJ*.4X~i",
+        database: "burgersDB"
+    });
+}
 // Make connection.
 connection.connect(function(err) {
     if (err) {
