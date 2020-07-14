6692082fefdb24ed8a3cd49ffda76fd6e1368a07
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-14 17:55:20 -0400

    fixing connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,12 @@
 // MYSQL ==================================================
 // ========================================================
 var mysql = require("mysql");
+let connection;
 // ----------
 if (process.env.JAWSDB_URL){
-    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    const connection = mysql.createConnection({
+    connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
